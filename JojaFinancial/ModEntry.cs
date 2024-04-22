9febf34a55728b22ece948e2883ab56e133bc368
SteveBenz <s8878992@hotmail.com>
2024-04-22 01:00:23 -0700

    Initial checkin with the visit from Morris coded up

---
+++ JojaFinancial/ModEntry.cs
@@ -0,0 +1,85 @@
+using Microsoft.Xna.Framework.Graphics;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewValley;
+
+namespace NermNermNerm.Junimatic
+{
+    public class ModEntry
+        : Mod, ISimpleLog
+    {
+        private const string StartLoanEventCommand = "JojaFinance.StartLoan";
+        private const string MorrisOffersLoanEvent = "JojaFinance.MorrisOffer";
+
+        public ModEntry()
+        {
+        }
+
+        public override void Entry(IModHelper helper)
+        {
+            this.Helper.Events.Content.AssetRequested += this.OnAssetRequested;
+            Event.RegisterCommand(StartLoanEventCommand, this.StartLoan);
+        }
+
+        private void StartLoan(Event @event, string[] args, EventContext context)
+        {
+
+            try
+            {
+                this.LogInfo("Loan initiated");
+            }
+            finally
+            {
+                ++@event.currentCommand;
+            }
+        }
+
+        private void OnAssetRequested(object? sender, AssetRequestedEventArgs e)
+        {
+            if (e.NameWithoutLocale.IsEquivalentTo("Data/Events/Farm"))
+            {
+                e.Edit((data) =>
+                {
+                    var dict = data.AsDictionary<string, string>().Data;
+                    dict[$"{MorrisOffersLoanEvent}/t 600 930/w sunny/d Mon Tue"] = $@"
+continue
+64 15
+farmer 64 15 2 Morris 65 16 0
+
+setskipactions addItem (BC)214
+skippable
+
+speak Morris ""Welcome to the Valley!  It is my pleasure to welcome you to our community on behalf of the whole Joja Team!#$b#Please accept this telephone as a housewarming gift from your friends at your local Jojamart!""
+addItem (BC)214
+speak Morris ""While I'm here, I thought I'd tell you about a SPECIAL OFFER, EXCLUSIVELY for new residents of Stardew Valley!#$b#We'd you to have a complete Wallpaper and Furniture Catalog for ABSOLUTELY NO MONEY DOWN and NO PAYMENTS for TWO MONTHS!$1""
+faceDirection Morris 3
+speak Morris ""...mumble mumble...  usurious interests rates...  mumble mumble...  unfair fees... mumble mumble... draconian penalties...  mumble mumble...$3""
+faceDirection Morris 0
+speak Morris ""SO ARE YOU READY TO START LIVING IN COMFORT??!  Sure you are!  Just sign this contract and I'll have that furniture catalog shipped right out!""
+quickQuestion #Morris, I am so ready to start living my dream!#Ermm..  I need time to think about that(break)emote Morris 32\speak Morris ""Great!  The Joja corporation is ready to enable you to live the way you want to, NOW!  Let the future take care of itself, am I right??!  I'll have those catalogs shipped out tonight!""\JojaFinance.StartLoan(break)emote Morris 12\speak Morris ""That's very... responsible of you - financial decisions like this should be undertaken with careful thought.$3#$b#JojaFinancial is ready whenever you are!  Just use your complimentary Joja Phone to call our offices after you've thought it over!""
+
+faceDirection Morris 1
+pause 200
+faceDirection Morris 0
+speak Morris ""Once again, Welcome to Stardew Valley and we look forward to seeing you at your local neighborhood Jojamart!""
+pause 200
+faceDirection Morris 1
+end fade
+".Replace("\r", "").Replace("\n", "/");
+                });
+            }
+        }
+
+        public void WriteToLog(string message, LogLevel level, bool isOnceOnly)
+        {
+            if (isOnceOnly)
+            {
+                this.Monitor.LogOnce(message, level);
+            }
+            else
+            {
+                this.Monitor.Log(message, level);
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,38 @@
+﻿# Joja Financial
+
+This mod is for people who are frustrated with not being able to get their house furnished
+without cheating.
+
+## Installing the mod
+
+Unpack the latest release into your Mods folder.  The folder structure should end up as `StardewValley\Mods\Junimatic`.
+
+## Playing with the mod
+
+Early in your playthrough, you'll get a visit from Morris, who offers you a loan to buy the
+furniture and wallpaper catalogs.  The payment terms are such that you don't pay much at all
+in the first year and the bulk is paid for in Fall and Winter of year 2.  You pay the debt
+by phone calls to JojaFinancial.
+
+## Translating the mods
+Until the mod reaches some basic level of stability, it's going to be English-only.
+
+## Compiling the mods
+Installing stable releases from Nexus Mods is recommended for most users. If you really want to
+compile the mod yourself, read on.
+
+These mods use the [crossplatform build config](https://www.nuget.org/packages/Pathoschild.Stardew.ModBuildConfig)
+so they can be built on Linux, Mac, and Windows without changes. See [the build config documentation](https://www.nuget.org/packages/Pathoschild.Stardew.ModBuildConfig)
+for troubleshooting.
+
+Build the project in [Visual Studio](https://www.visualstudio.com/vs/community/) or [MonoDevelop](https://www.monodevelop.com/) to
+build it and deploy it to your 'mod' directory in your Stardew Valley installation.
+
+Launching it under the debugger will start Stardew Valley and your mod will be picked up as in the game.
+
+### Compiling a mod for release
+To package a mod for release:
+
+1. Switch to `Release` build configuration.
+2. Recompile the mod per the previous section.
+3. Upload the generated `bin/Release/<mod name>-<version>.zip` file from the project folder.
